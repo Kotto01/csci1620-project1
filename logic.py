895b84e6763bfda8ec76860c556686459c4033e4
Kotto01 <kotslitemail@gmail.com>
2024-05-11 07:23:17 -0500

    holy moly

--- logic.py
+++ logic.py
@@ -2,6 +2,6 @@
 votes = {"Bianca": 0, "Felicia": 0, "Edward": 0}
 
 def print_votes():
-    print("Final Vote Tally:")
+    print("\nFinal Vote Tally:")
     for candidate, vote_count in votes.items():
         print(f"{candidate}: {vote_count} votes")
